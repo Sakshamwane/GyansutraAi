369dabcc8b809ada3ed99531ed7cd356a7aae81b
Saksham Wane <wanesaksham@gmail.com>
2026-04-25 13:31:59 +0530

    Fix Vercel module path for backend

--- server/vercel_wsgi.py
+++ server/vercel_wsgi.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the server directory to the python path
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 app = get_wsgi_application()
